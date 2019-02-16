cd9d0884497283b18ba3907f7f644b5f1ebf9ec1
chashchina_av <chashchina_av@taximaxim.local>
2019-02-16 19:31:45 +0200

    first commite

---
+++ src/app/app.component.ts
@@ -0,0 +1,181 @@
+import { Component } from '@angular/core';
+declare var $: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  imgs = [
+    { url: 'https://images.pexels.com/photos/723240/pexels-photo-723240.jpeg' },
+    { url: 'https://images.pexels.com/photos/46160/field-clouds-sky-earth-46160.jpeg'},
+    { url: 'https://images.pexels.com/photos/215/road-sky-clouds-cloudy.jpg' },
+    { url: 'https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg' },
+    { url: 'https://a.radikal.ru/a15/1902/af/60e2b65e3674.png' }
+  ];
+  currentImgSrc = {url: ''};
+  mouse = {
+    x: 0,
+    y: 0,
+    startX: 0,
+    startY: 0
+  };
+
+  closeModal() {
+    this.mouse = {
+      x: 0,
+      y: 0,
+      startX: 0,
+      startY: 0
+    };
+    $('.rectangle').remove();
+    $('.txt-input').remove();
+
+  }
+
+  showModal(ev) {
+    $('#modal').modal('show');
+    this.currentImgSrc.url = ev.target.src;
+    const canvas = $('#imgCanvas')[0];
+    const img = new Image();
+    const ctx = canvas.getContext('2d');
+    const canvasCopy = document.createElement('canvas');
+    const copyctx = canvasCopy.getContext('2d');
+    img.src = ev.target.src;
+    img.onload = function() {
+      let ratio = 1;
+      const maxWidth = 900;
+      const maxHeight = 600;
+
+      if (img.width > maxWidth) {
+        ratio = maxWidth / img.width;
+      } else if (img.height > maxHeight) {
+        ratio = maxHeight / img.height;
+      }
+
+      canvasCopy.width = img.width;
+      canvasCopy.height = img.height;
+      copyctx.drawImage(img, 0, 0);
+
+      canvas.width = img.width * ratio;
+      canvas.height = img.height * ratio;
+      ctx.drawImage(
+        canvasCopy,
+        0,
+        0,
+        canvasCopy.width,
+        canvasCopy.height,
+        0,
+        0,
+        canvas.width,
+        canvas.height
+      );
+    };
+  }
+
+  cutImg() {
+    let that = this;
+    const canvas = $('#imgCanvas')[0];
+    const ctx = canvas.getContext('2d');
+    let widthX = this.mouse.x - this.mouse.startX;
+    let heihgtY = this.mouse.y - this.mouse.startY;
+    let imgData = ctx.getImageData(this.mouse.startX, this.mouse.startY, widthX, heihgtY);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.putImageData(imgData, 0, 0);
+    $('.rectangle').remove();
+    canvas.onclick = null;
+  }
+
+ initDraw(canvas) {
+   let that = this;
+   const modalBody = $('#modal-body')[0];
+    function setMousePosition(e) {
+        const ev = e || window.event;
+        mouse.x = ev.offsetX;
+        mouse.y = ev.offsetY;
+     }
+
+    let element = null;
+    let mouse = {
+      x: 0,
+      y: 0,
+      startX: 0,
+      startY: 0
+    };
+    // if (modalBody.onclick !== null) {
+    //   modalBody.onclick = null;
+    // }
+
+    canvas.onmousemove = function  (e) {
+        setMousePosition(e);
+        if (element !== null) {
+            element.style.width = Math.abs(mouse.x - mouse.startX) + 'px';
+            element.style.height = Math.abs(mouse.y - mouse.startY) + 'px';
+            element.style.left = (mouse.x - mouse.startX < 0) ? mouse.x + 'px' : mouse.startX + 'px';
+            element.style.top = (mouse.y - mouse.startY < 0) ? mouse.y + 'px' : mouse.startY + 'px';
+        }
+    };
+
+    canvas.onclick = function (e) {
+        if (element !== null) {
+            element = null;
+            canvas.style.cursor = 'default';
+            canvas.onmousemove = function() {};
+            that.mouse = mouse;
+            console.log('finsihed.', mouse);
+        } else {
+            console.log('begun.');
+            mouse.startX = mouse.x;
+            mouse.startY = mouse.y;
+            element = document.createElement('div');
+            element.className = 'rectangle';
+            element.style.border = '1px solid #FF0000';
+            element.style.position = 'absolute';
+            element.style.left = mouse.x + 'px';
+            element.style.top = mouse.y + 'px';
+
+            modalBody.appendChild(element);
+            canvas.style.cursor = 'crosshair';
+        }
+    };
+}
+
+  getTxtInput(ev) {
+    const modalBody = $('#modal-body')[0];
+    let cellText = null;
+    modalBody.onclick = function (ev) {
+      if (cellText == null) {
+        cellText = $('<input/>');
+        cellText[0].className = 'txt-input';
+        cellText[0].style.position = 'absolute';
+        cellText[0].style.left = ev.offsetX + 'px';
+        cellText[0].style.top = ev.offsetY + 'px';
+        cellText[0].style.width = '200px';
+        cellText[0].style.color = 'red';
+        cellText[0].style['background-color'] = 'inherit';
+        cellText.type = 'text';
+        modalBody.append(cellText[0]);
+      } else {
+        cellText = null;
+      }
+    };
+  }
+
+  saveTxt() {
+    const inputsArr = $('.txt-input');
+    const canvas = $('#imgCanvas')[0];
+    const ctx = canvas.getContext('2d');
+    for (let i = 0; i < inputsArr.length; i++) {
+      const element = inputsArr[i];
+      ctx.lineWidth = 1;
+      ctx.fillStyle = 'red';
+      ctx.lineStyle = '#ffff00';
+      ctx.font = '18px sans-serif';
+      const pos = $('.txt-input').position();
+      ctx.fillText(element.value, pos.left, (pos.top + (element.size / 2)));
+      element.remove();
+    }
+    $('#modal-body')[0].onclick = null;
+  }
+}
