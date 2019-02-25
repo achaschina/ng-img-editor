f6dd45eb7226792e703165027c47d733151f4214
chashchina_av <chashchina_av@taximaxim.local>
2019-02-25 11:07:14 +0200

    changed input text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,17 @@ declare var $: any;
 export class AppComponent {
   imgs = [
     { url: 'https://images.pexels.com/photos/723240/pexels-photo-723240.jpeg' },
-    { url: 'https://images.pexels.com/photos/46160/field-clouds-sky-earth-46160.jpeg'},
+    {
+      url:
+        'https://images.pexels.com/photos/46160/field-clouds-sky-earth-46160.jpeg'
+    },
     { url: 'https://images.pexels.com/photos/215/road-sky-clouds-cloudy.jpg' },
-    { url: 'https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg' },
+    {
+      url: 'https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg'
+    },
     { url: 'https://a.radikal.ru/a15/1902/af/60e2b65e3674.png' }
   ];
-  currentImgSrc = {url: ''};
+  currentImgSrc = { url: '' };
   mouse = {
     x: 0,
     y: 0,
@@ -31,7 +36,6 @@ export class AppComponent {
     };
     $('.rectangle').remove();
     $('.txt-input').remove();
-
   }
 
   showModal(ev) {
@@ -75,29 +79,34 @@ export class AppComponent {
   }
 
   cutImg() {
-    let that = this;
+    const that = this;
     const canvas = $('#imgCanvas')[0];
     const ctx = canvas.getContext('2d');
-    let widthX = this.mouse.x - this.mouse.startX;
-    let heihgtY = this.mouse.y - this.mouse.startY;
-    let imgData = ctx.getImageData(this.mouse.startX, this.mouse.startY, widthX, heihgtY);
+    const widthX = this.mouse.x - this.mouse.startX;
+    const heihgtY = this.mouse.y - this.mouse.startY;
+    const imgData = ctx.getImageData(
+      this.mouse.startX,
+      this.mouse.startY,
+      widthX,
+      heihgtY
+    );
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.putImageData(imgData, 0, 0);
     $('.rectangle').remove();
     canvas.onclick = null;
   }
 
- initDraw(canvas) {
-   let that = this;
-   const modalBody = $('#modal-body')[0];
+  initDraw(canvas) {
+    const that = this;
+    const modalBody = $('#modal-body')[0];
     function setMousePosition(e) {
-        const ev = e || window.event;
-        mouse.x = ev.offsetX;
-        mouse.y = ev.offsetY;
-     }
+      const ev = e || window.event;
+      mouse.x = ev.offsetX;
+      mouse.y = ev.offsetY;
+    }
 
     let element = null;
-    let mouse = {
+    const mouse = {
       x: 0,
       y: 0,
       startX: 0,
@@ -107,46 +116,49 @@ export class AppComponent {
     //   modalBody.onclick = null;
     // }
 
-    canvas.onmousemove = function  (e) {
-        setMousePosition(e);
-        if (element !== null) {
-            element.style.width = Math.abs(mouse.x - mouse.startX) + 'px';
-            element.style.height = Math.abs(mouse.y - mouse.startY) + 'px';
-            element.style.left = (mouse.x - mouse.startX < 0) ? mouse.x + 'px' : mouse.startX + 'px';
-            element.style.top = (mouse.y - mouse.startY < 0) ? mouse.y + 'px' : mouse.startY + 'px';
-        }
+    canvas.onmousemove = function(e) {
+      setMousePosition(e);
+      if (element !== null) {
+        element.style.width = Math.abs(mouse.x - mouse.startX) + 'px';
+        element.style.height = Math.abs(mouse.y - mouse.startY) + 'px';
+        element.style.left =
+          mouse.x - mouse.startX < 0 ? mouse.x + 'px' : mouse.startX + 'px';
+        element.style.top =
+          mouse.y - mouse.startY < 0 ? mouse.y + 'px' : mouse.startY + 'px';
+      }
     };
 
-    canvas.onclick = function (e) {
-        if (element !== null) {
-            element = null;
-            canvas.style.cursor = 'default';
-            canvas.onmousemove = function() {};
-            that.mouse = mouse;
-            console.log('finsihed.', mouse);
-        } else {
-            console.log('begun.');
-            mouse.startX = mouse.x;
-            mouse.startY = mouse.y;
-            element = document.createElement('div');
-            element.className = 'rectangle';
-            element.style.border = '1px solid #FF0000';
-            element.style.position = 'absolute';
-            element.style.left = mouse.x + 'px';
-            element.style.top = mouse.y + 'px';
+    canvas.onclick = function(e) {
+      if (element !== null) {
+        element = null;
+        canvas.style.cursor = 'default';
+        canvas.onmousemove = function() {};
+        that.mouse = mouse;
+        console.log('finsihed.', mouse);
+      } else {
+        console.log('begun.');
+        mouse.startX = mouse.x;
+        mouse.startY = mouse.y;
+        element = document.createElement('div');
+        element.className = 'rectangle';
+        element.style.border = '1px solid #FF0000';
+        element.style.position = 'absolute';
+        element.style.left = mouse.x + 'px';
+        element.style.top = mouse.y + 'px';
 
-            modalBody.appendChild(element);
-            canvas.style.cursor = 'crosshair';
-        }
+        modalBody.appendChild(element);
+        canvas.style.cursor = 'crosshair';
+      }
     };
-}
+  }
 
   getTxtInput(ev) {
     const modalBody = $('#modal-body')[0];
     let cellText = null;
-    modalBody.onclick = function (ev) {
+    modalBody.onclick = function(ev) {
+      console.log(cellText)
       if (cellText == null) {
-        cellText = $('<input/>');
+        cellText = $('<textarea/>');
         cellText[0].className = 'txt-input';
         cellText[0].style.position = 'absolute';
         cellText[0].style.left = ev.offsetX + 'px';
@@ -154,10 +166,7 @@ export class AppComponent {
         cellText[0].style.width = '200px';
         cellText[0].style.color = 'red';
         cellText[0].style['background-color'] = 'inherit';
-        cellText.type = 'text';
         modalBody.append(cellText[0]);
-      } else {
-        cellText = null;
       }
     };
   }
@@ -168,12 +177,41 @@ export class AppComponent {
     const ctx = canvas.getContext('2d');
     for (let i = 0; i < inputsArr.length; i++) {
       const element = inputsArr[i];
-      ctx.lineWidth = 1;
-      ctx.fillStyle = 'red';
-      ctx.lineStyle = '#ffff00';
-      ctx.font = '18px sans-serif';
-      const pos = $('.txt-input').position();
-      ctx.fillText(element.value, pos.left, (pos.top + (element.size / 2)));
+      if (element.value !== '') {
+        ctx.lineWidth = 1;
+        ctx.fillStyle = 'red';
+        ctx.lineStyle = '#ffff00';
+        ctx.font = '18px sans-serif';
+        const pos = $('.txt-input').position();
+
+        const printAt = function(context, text, x, y, lineHeight, fitWidth) {
+          fitWidth = fitWidth || 0;
+
+          if (fitWidth <= 0) {
+            context.fillText(text, x, y);
+            return;
+          }
+
+          for (let idx = 1; idx <= text.length; idx++) {
+            const str = text.substr(0, idx);
+            // console.log(str, context.measureText(str).width, fitWidth);
+            if (context.measureText(str).width > fitWidth) {
+              context.fillText(text.substr(0, idx - 1), x, y);
+              printAt(
+                context,
+                text.substr(idx - 1),
+                x,
+                y + lineHeight,
+                lineHeight,
+                fitWidth
+              );
+              return;
+            }
+          }
+          context.fillText(text, x, y);
+        };
+        printAt(ctx, element.value, pos.left, pos.top + 18, 15, 200 );
+      }
       element.remove();
     }
     $('#modal-body')[0].onclick = null;
