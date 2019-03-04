d0a5eab1c41db1a84f91dad511ac4c222bfc7c18
chashchina_av <chashchina_av@taximaxim.local>
2019-03-04 19:37:18 +0200

    add selected area

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,6 +21,12 @@ export class AppComponent {
     startX: 0,
     startY: 0
   };
+  mouseSqr = {
+    x: 0,
+    y: 0,
+    startX: 0,
+    startY: 0
+  };
 
   closeModal() {
     this.mouse = {
@@ -29,8 +35,247 @@ export class AppComponent {
       startX: 0,
       startY: 0
     };
+    this.mouseSqr = {
+      x: 0,
+      y: 0,
+      startX: 0,
+      startY: 0
+    };
     $('.resizable').remove();
+    $('.resizable-square').remove();
     $('.txt-input').remove();
+  }
+
+  selectArea(canvas) {
+    // $('.resizable').remove();
+    const that = this;
+    const modalBody = $('#modal-body')[0];
+    function setMousePosition(e) {
+      const ev = e || window.event;
+      mouseSqr.x = ev.offsetX;
+      mouseSqr.y = ev.offsetY;
+    }
+
+    let element = null;
+    let resizers = null;
+    let resizer = null;
+
+    const mouseSqr = {
+      x: 0,
+      y: 0,
+      startX: 0,
+      startY: 0
+    };
+
+    function makeResizableDiv(element) {
+      const resizers = document.querySelectorAll('.resizer');
+      const minimum_size = 20;
+      let original_width = 0;
+      let original_height = 0;
+      let original_x = 0;
+      let original_y = 0;
+      let original_mouse_x = 0;
+      let original_mouse_y = 0;
+
+      for (let i = 0; i < resizers.length; i++) {
+        const currentResizer = resizers[i];
+        currentResizer.addEventListener('mousedown', function(e: MouseEvent) {
+          e.preventDefault();
+          original_width = parseFloat(
+            getComputedStyle(element, null)
+              .getPropertyValue('width')
+              .replace('px', '')
+          );
+          original_height = parseFloat(
+            getComputedStyle(element, null)
+              .getPropertyValue('height')
+              .replace('px', '')
+          );
+          original_x = element.getBoundingClientRect().left;
+          original_y = element.getBoundingClientRect().top;
+          original_mouse_x = e.pageX;
+          original_mouse_y = e.pageY;
+
+          function resize(e: MouseEvent) {
+            if (currentResizer.classList.contains('bottom-right')) {
+              // mouse.x = e.pageX;
+              // mouse.y = e.pageY;
+              // element.style.width = mouse.x - element.getBoundingClientRect().left + 'px';
+              const width = original_width + (e.pageX - original_mouse_x);
+              const height = original_height + (e.pageY - original_mouse_y);
+              if (width > minimum_size) {
+                element.style.width = width + 'px';
+              }
+              if (height > minimum_size) {
+                element.style.height = height + 'px';
+              }
+            } else if (currentResizer.classList.contains('bottom-left')) {
+              const height = original_height + (e.pageY - original_mouse_y);
+              const width = original_width - (e.pageX - original_mouse_x);
+              if (height > minimum_size) {
+                element.style.height = height + 'px';
+              }
+              if (width > minimum_size) {
+                element.style.width = width + 'px';
+                element.style.left =
+                  mouseSqr.startX + (e.pageX - original_mouse_x) + 'px';
+              }
+            } else if (currentResizer.classList.contains('top-right')) {
+              const width = original_width + (e.pageX - original_mouse_x);
+              const height = original_height - (e.pageY - original_mouse_y);
+              if (width > minimum_size) {
+                element.style.width = width + 'px';
+              }
+              if (height > minimum_size) {
+                element.style.height = height + 'px';
+                element.style.top =
+                  mouseSqr.startY + (e.pageY - original_mouse_y) + 'px';
+              }
+            } else {
+              const width = original_width - (e.pageX - original_mouse_x);
+              const height = original_height - (e.pageY - original_mouse_y);
+              if (width > minimum_size) {
+                element.style.width = width + 'px';
+                element.style.left =
+                  mouseSqr.startX + (e.pageX - original_mouse_x) + 'px';
+              }
+              if (height > minimum_size) {
+                element.style.height = height + 'px';
+                element.style.top =
+                  mouseSqr.startY + (e.pageY - original_mouse_y) + 'px';
+              }
+            }
+          }
+
+          function stopResize() {
+            window.removeEventListener('mousemove', resize);
+            const newElement = $('.resizable-square')[0];
+            if (newElement) {
+              mouseSqr.x = newElement.offsetWidth + newElement.offsetLeft;
+              mouseSqr.y = newElement.offsetTop + newElement.offsetHeight;
+              mouseSqr.startX = newElement.offsetLeft;
+              mouseSqr.startY = newElement.offsetTop;
+            }
+          }
+
+          window.addEventListener('mousemove', resize);
+          window.addEventListener('mouseup', stopResize);
+        });
+      }
+    }
+
+    canvas.onmousemove = function(e) {
+      setMousePosition(e);
+      if (element !== null) {
+        element.style.width = Math.abs(mouseSqr.x - mouseSqr.startX) + 'px';
+        element.style.height = Math.abs(mouseSqr.y - mouseSqr.startY) + 'px';
+        element.style.left =
+          mouseSqr.x - mouseSqr.startX < 0 ? mouseSqr.x + 'px' : mouseSqr.startX + 'px';
+        element.style.top =
+          mouseSqr.y - mouseSqr.startY < 0 ? mouseSqr.y + 'px' : mouseSqr.startY + 'px';
+      }
+    };
+
+    function createReizers(el) {
+      resizers = document.createElement('div');
+      resizers.className = 'resizers';
+      resizers.style.width = '100%';
+      resizers.style.height = '100%';
+      resizers.style.position = 'absolute';
+      resizers.style['box-sizing'] = 'border-box';
+      element.appendChild(resizers);
+
+      resizer = document.createElement('div');
+      resizer.className = 'resizer top-left';
+      resizer.style.left = '-5px';
+      resizer.style.top = '-5px';
+      resizer.style.width = '10px';
+      resizer.style.height = '10px';
+      // resizer.style['border-radius'] = '50%';
+      resizer.style.position = 'absolute';
+      resizer.style.border = '1px solid #FF0000';
+      resizer.style.background = '#FF0000';
+      resizer.style.cursor = 'nwse-resize';
+      resizers.appendChild(resizer);
+
+      resizer = document.createElement('div');
+      resizer.className = 'resizer top-right';
+      resizer.style.right = '-5px';
+      resizer.style.top = '-5px';
+      resizer.style.width = '10px';
+      resizer.style.height = '10px';
+      // resizer.style['border-radius'] = '50%';
+      resizer.style.position = 'absolute';
+      resizer.style.border = '3px solid #FF0000';
+      resizer.style.background = '#FF0000';
+      resizer.style.cursor = 'nesw-resize';
+      resizers.appendChild(resizer);
+
+      resizer = document.createElement('div');
+      resizer.className = 'resizer bottom-right';
+      resizer.style.right = '-5px';
+      resizer.style.bottom = '-5px';
+      resizer.style.width = '10px';
+      resizer.style.height = '10px';
+      // resizer.style['border-radius'] = '50%';
+      resizer.style.position = 'absolute';
+      resizer.style.border = '3px solid #FF0000';
+      resizer.style.background = '#FF0000';
+      resizer.style.cursor = 'nwse-resize';
+      resizers.appendChild(resizer);
+
+      resizer = document.createElement('div');
+      resizer.className = 'resizer bottom-left';
+      resizer.style.left = '-5px';
+      resizer.style.bottom = '-5px';
+      resizer.style.width = '10px';
+      resizer.style.height = '10px';
+      // resizer.style['border-radius'] = '50%';
+      resizer.style.position = 'absolute';
+      resizer.style.border = '3px solid #FF0000';
+      resizer.style.background = '#FF0000';
+      resizer.style.cursor = 'nesw-resize';
+      resizers.appendChild(resizer);
+    }
+
+    canvas.onclick = function(e) {
+      if (element !== null) {
+        resizers = null;
+        resizer = null;
+        canvas.style.cursor = 'default';
+        canvas.onmousemove = function() {};
+        that.mouse = mouseSqr;
+        console.log('finsihed.', mouseSqr);
+
+        createReizers(element);
+        makeResizableDiv(element);
+        element = null;
+      } else {
+        console.log('begun.');
+        mouseSqr.startX = mouseSqr.x;
+        mouseSqr.startY = mouseSqr.y;
+        element = document.createElement('div');
+        element.className = 'resizable-square';
+        element.style.border = '2px solid #FF0000';
+        element.style.position = 'absolute';
+        element.style.left = mouseSqr.x + 'px';
+        element.style.top = mouseSqr.y + 'px';
+
+        modalBody.appendChild(element);
+        canvas.style.cursor = 'crosshair';
+      }
+    };
+  }
+
+  saveSqr() {
+    const squares = $('.resizable-square');
+    const canvas = $('#imgCanvas')[0];
+    const ctx = canvas.getContext('2d');
+    for (let i = 0; i < squares.length; i++) {
+      ctx.strokeStyle = 'red';
+      ctx.strokeRect(squares[i].offsetLeft, squares[i].offsetTop, squares[i].clientWidth, squares[i].clientHeight);
+      squares[i].remove();
+    }
   }
 
   showModal(ev) {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatInputModule} from '@angular/material';
+import { MatIconModule, MatButtonModule, } from '@angular/material';
+import 'hammerjs';
 import * as $ from 'jquery';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
+  declarations: [AppComponent],
+  imports: [BrowserModule,
+    BrowserAnimationsModule,
+    MatTooltipModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
